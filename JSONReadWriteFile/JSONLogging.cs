4fea2a3de603d935a6a2215d8ab347759b466e17
Thabang Matladi <thabang@sovereigntysystems.co.za>
2020-05-04 19:48:46 +0200

    Serialize Object into a JSON File

---
+++ JSONReadWriteFile/JSONLogging.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+namespace AirfcraftBasicFlight_v2
+{
+    class JSONLogging
+    {
+        public void SaveSimulated(List<DataLog> dataToSave)
+        {
+            // Use the current time to set the File Name
+            DateTime time = DateTime.Now;
+            string fileName = time.Year.ToString() + time.Month.ToString() + time.Day.ToString() + "_" + time.Hour.ToString() +
+                time.Minute.ToString() + time.Second.ToString();
+            string filePath = "C:\\WorkSpace\\Programming\\SOV\\AirfcraftBasicFlight_v2\\" + fileName + ".json";
+            
+            // Serialize JSON directly to a file
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(dataToSave));
+        }
+    }
+}
